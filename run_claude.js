b3ec6c8b2e74d515c9d7a656b2e3366ac9031ac7
qq939 <qq939@users.noreply.github.com>
2026-06-04 23:56:44 +0800

    fix: remove run_claude.js from repo, add to gitignore - container system file

--- run_claude.js
+++
@@ -1,3 +0,0 @@
-// Stub - prevents "Cannot find module" error on container startup
-// Actual server is started by user_start.sh
-console.log('run_claude.js stub: Mythos server managed by user_start.sh');
